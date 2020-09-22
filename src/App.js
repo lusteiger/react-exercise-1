f562cbb83ab5a90d63c58764731f721ad7897c7f
lusteiger <leonardoxy7@gmail.com>
2020-09-22 23:41:07 +0800

    [xinyu lu] implement avator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import avatarSrc from './assets/avatar.jpg';
 import './App.scss';
-
+import { Avatar } from './compoments/Avatar/Avatar';
 class App extends Component {
   render() {
-    return <main className="app">start here</main>;
+    return (
+      <main className="app">
+        <Avatar src={avatarSrc} />
+      </main>
+    );
   }
 }
 
