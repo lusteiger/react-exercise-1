0eeb5f3037dc52f1f8ae12bd0955f195d3c4275d
lusteiger <leonardoxy7@gmail.com>
2020-09-23 08:37:31 +0800

    [xinyu lu] implement the introduction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import avatarSrc from './assets/avatar.jpg';
 import './App.scss';
 import { Avatar } from './compoments/Avatar/Avatar';
 import { Headline } from './compoments/Headline/headline';
+import { Introduction } from './compoments/Introduction/Introduction';
+import { Resume } from './compoments/Resume/Resume';
 
 const title = 'Hello,';
 const description = 'my name is kamil 24yo and this is my resume/cv';
@@ -12,6 +14,17 @@ class App extends Component {
       <main className="app">
         <Avatar src={avatarSrc} />
         <Headline title={title} description={description} />
+
+        <Introduction />
+
+        <Resume title="About me">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            aut placeat quam deserunt, pariatur nam ad commodi modi voluptas quo
+            ratione quia molestias nesciunt debitis ab quae. Sit, reprehenderit
+            aliquam.
+          </p>
+        </Resume>
       </main>
     );
   }
---
+++ src/compoments/Introduction/Introduction.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Introduction.scss';
+export function Introduction() {
+  return <hr className="Introdution" />;
+}
