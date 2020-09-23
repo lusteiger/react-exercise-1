e34c188a7439f91a501c370e4aa63f4696719150
lusteiger <leonardoxy7@gmail.com>
2020-09-23 09:01:18 +0800

    [xinyu lu] implement the education

--- src/App.js
+++ src/App.js
@@ -5,9 +5,45 @@ import { Avatar } from './compoments/Avatar/Avatar';
 import { Headline } from './compoments/Headline/headline';
 import { Introduction } from './compoments/Introduction/Introduction';
 import { Resume } from './compoments/Resume/Resume';
+import { Education } from './compoments/Education/education';
 
 const title = 'Hello,';
 const description = 'my name is kamil 24yo and this is my resume/cv';
+
+const educations = [
+  {
+    id: 1,
+    year: 1990,
+    title: 'I was born in Katowice',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatemaut placeat quam deserunt, pariatur nam ad commodi modi',
+  },
+
+  {
+    id: 2,
+    year: 2005,
+    title: 'Secondary school specializing in artistic',
+    description:
+      'pariatur nam ad commodi modi voluptas quoratione quia molestias nesciunt debitis ab quae. Sit, reprehenderit aliquam',
+  },
+
+  {
+    id: 3,
+    year: 2009,
+    title: 'First level graduation in Graphic Design',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatemaut placeat quam deserunt, pariatur nam ad commodi modi',
+  },
+
+  {
+    id: 4,
+    year: 2012,
+    title: 'Second level graduation in Graphic Design',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatemaut placeat quam deserunt, pariatur nam ad commodi modi',
+  },
+];
+
 class App extends Component {
   render() {
     return (
@@ -24,6 +60,12 @@ class App extends Component {
             ratione quia molestias nesciunt debitis ab quae. Sit, reprehenderit
             aliquam.
           </p>
+        </Resume>
+
+        <Resume title="Education">
+          {educations.map((education) => (
+            <Education key={education.id} education={education} />
+          ))}
         </Resume>
       </main>
     );
